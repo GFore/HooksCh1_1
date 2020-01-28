7743ca8b21e04a9e96a63a56ab6c65d12fc1de7b
Greg Foreman <gfore@mindspring.com>
2020-01-27 23:51:54 -0500

    practice with Memo

---
+++ src/Count.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Count() {
+  const [count, setCount] = React.useState(0);
+  const renders = React.useRef(0);
+  return (
+    <div className='App'>
+      <div>count: {count}</div>
+      <div>renders: {renders.current++}</div>
+      <button onClick={() => setCount(c => c + 1)}>increment</button>
+    </div>
+  );
+}
